a84b67520ed50756a7b11896a208b94f02955e25
Ezra Leong <ezrawork20@gmail.com>
2024-02-24 16:30:30 +0800

    testing mount component for themeswitch

--- app/components/ThemesSwitch.tsx
+++ app/components/ThemesSwitch.tsx
@@ -1,21 +1,22 @@
-"use client"
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 import { SunIcon, MoonIcon } from './Icons';
 
 const ThemesSwitch = () => {
-  const {theme, setTheme} = useTheme();
+  const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+
+  // Immediately mount component on client side
   useEffect(() => {
-    setMounted(true)
-  }, [])
+    setMounted(true);
+  }, []);
 
-  if (!mounted) {
-    return null;
-  }
-
-  return <button onClick={() => setTheme(theme === 'dark'? 'light': 'dark')}>
-      {theme === 'dark' ? <SunIcon/> : <MoonIcon/>}</button>;
+  // Always render the button, but conditionally render its children based on the mounted state
+  return (
+    <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+      {mounted && (theme === 'dark' ? <SunIcon /> : <MoonIcon />)}
+    </button>
+  );
 };
 
-export default ThemesSwitch
+export default ThemesSwitch;
