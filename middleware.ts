a44d53efb136f35455a53331b298f2ce9fec49a4
Ezra Leong <ezrawork20@gmail.com>
2024-03-03 17:09:46 +0800

    putting nonce safeguard

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+    // Generate a nonce for inline scripts and styles
+    const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+
+    // Initialize the response to proceed with the request
     const response = NextResponse.next();
 
+    // Security Headers
     // 1. Strict Transport Security (HSTS)
     response.headers.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
 
-    // 3. X-Frame-Options
+    // 2. X-Frame-Options
     response.headers.set("X-Frame-Options", "DENY");
 
-    // 4. Referrer-Policy
+    // 3. Referrer-Policy
     response.headers.set("Referrer-Policy", "no-referrer-when-downgrade");
 
-    // 5. Permissions-Policy
+    // 4. Permissions-Policy
     response.headers.set("Permissions-Policy", "microphone=()");
+
+    // 5. Content Security Policy (CSP)
+    const cspHeader = `
+        default-src 'self';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+        style-src 'self' 'nonce-${nonce}';
+        img-src 'self' blob: data:;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        block-all-mixed-content;
+        upgrade-insecure-requests;
+    `.replace(/\s{2,}/g, " ").trim();
+
+    response.headers.set("Content-Security-Policy", cspHeader);
 
     return response;
 }
