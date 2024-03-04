48c5d5cc7c692f7a089d6a1204e2709941beeb62
Ezra Leong <ezrawork20@gmail.com>
2024-03-04 16:17:05 +0800

    cleaned route.ts

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import { Metadata } from 'next';
 import Footer from '../components/Footer';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/api/routes/route.ts
+++ app/api/routes/route.ts
@@ -1,10 +1,7 @@
 import { SEODocument } from "../../components/LeftSideNavbar";
 import { client } from "@/sanity/lib/client";
 import { NextRequest, NextResponse } from "next/server";
-import { setCache, getCache } from "../../utils.tsx/cache";
-//Caching mechanism is completely invalidated due to serverless function that eliminates the memory file once the instance is completed. LOL
-// At the top of your route.ts file 
-const ongoingFetches = new Map();
+import { fetchDataWithLock } from "../../utils.tsx/cache";
 
 async function fetchSEOLinksTitles() {
     const query = `
@@ -17,40 +14,19 @@ async function fetchSEOLinksTitles() {
     return documents;
 }
 
-// Modified GET function
+// Simplified GET function using fetchDataWithLock
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const cacheKey = 'seoLinksTitles';
-  let data = getCache(cacheKey);
+  
+  // Use fetchDataWithLock to either get the cached data or fetch new data if not cached or expired.
+  const data = await fetchDataWithLock(cacheKey, fetchSEOLinksTitles, 3600); // TTL set to 3600 seconds (1 hour)
 
-  // Check for an ongoing fetch
-  if (!data) {
-    if (!ongoingFetches.has(cacheKey)) {
-      // If no ongoing fetch, start one and store the promise
-      const fetchPromise = fetchSEOLinksTitles().then(fetchedData => {
-        setCache(cacheKey, fetchedData, 3600); // Cache data for 1 hour
-        ongoingFetches.delete(cacheKey); // Clean up
-        return fetchedData;
-      }).catch(error => {
-        console.error('Failed to fetch SEO links titles:', error);
-        ongoingFetches.delete(cacheKey); // Ensure to clean up on failure too
-        throw error; // Re-throw to handle outside
-      });
-      
-      ongoingFetches.set(cacheKey, fetchPromise);
-      data = await fetchPromise; // Await the fetch promise
-    } else {
-      // If there's an ongoing fetch, wait for it
-      data = await ongoingFetches.get(cacheKey);
-    }
-  }
-
-  // Return the data in a NextResponse with Cache-Control header
+  // Return the data in a NextResponse with appropriate headers
   return new NextResponse(JSON.stringify(data), {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
-      // Example Cache-Control header for caching; adjust max-age as needed
-      'Cache-Control': 'public, max-age=3600, must-revalidate'
+      'Cache-Control': 'public, max-age=3600, must-revalidate', // Cache-Control header for client-side caching
     },
   });
 }
