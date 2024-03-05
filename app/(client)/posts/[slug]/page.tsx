93be5004c6395da89d57cc91b00e04bc964d3b65
Ezra Leong <ezrawork20@gmail.com>
2024-03-05 14:41:23 +0800

    added functionality to responsivesidebar

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -220,7 +220,7 @@ const page = async ({params}: Params) => {
         </article>
         <ResponsiveSidebarWrapper>
           {headings && headings.length > 0 && (
-            <div className="sticky top-32 lg:max-h-[calc(100vh*4/6)] lg:overflow-auto text-sm custom-scrollbar flex-shrink-0 w-60 hidden lg:block">
+            <div className="sticky top-32 lg:max-h-[calc(100vh*4/6)] lg:overflow-auto text-sm custom-scrollbar flex-shrink-0 w-60">
                 <TableOfContents headings={headings}/>
             </div>
           )}
