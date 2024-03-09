a7364f86ec563cfeceb26541c244bf8524b1351f
Ezra Leong <ezrawork20@gmail.com>
2024-03-09 16:28:43 +0800

    removed mount component

--- app/components/ThemesSwitch.tsx
+++ app/components/ThemesSwitch.tsx
@@ -1,23 +1,15 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTheme } from 'next-themes';
 import { SunIcon, MoonIcon } from './Icons';
 
 const ThemesSwitch = () => {
   const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return(null)
-  }
-
-  // Determine the appropriate label based on the current theme
+  // Directly determine the appropriate label based on the current theme
   const buttonLabel = theme === 'dark' ? 'Switch to light theme' : 'Switch to dark theme';
 
+  // Render the switch button directly without checking for mounted state
   return (
     <button 
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
