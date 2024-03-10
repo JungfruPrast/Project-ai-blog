bdd224d2ff99269dcc97c7d8d9bc89a83579b96f
Ezra Leong <ezrawork20@gmail.com>
2024-03-10 12:41:42 +0800

    Forgot to place headers get in the export statement. Fixed.

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,27 +1,28 @@
-import CMSNavbar from "../components/CMSNavbar"
-import Provider from "../utils.tsx/Provider"
-import { headers } from "next/headers"
-
-const nonce = headers().get('x-nonce') || ""
+import CMSNavbar from "../components/CMSNavbar";
+import Provider from "../utils.tsx/Provider";
+import { headers } from "next/headers";
 
 export const metadata = {
   title: 'Project AI Blog',
   description: 'Documenting my AI blogging Journey with ChatGPT4: From A to Z ',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  // Move the nonce retrieval inside the component where the headers() function has access to the request context
+  const nonce = headers().get('x-nonce') || "";
+
   return (
     <html lang="en">
       <body>
         <Provider nonce={nonce}>
-      <CMSNavbar/>
-        {children}
+          <CMSNavbar/>
+          {children}
         </Provider>
-        </body>
+      </body>
     </html>
-  )
+  );
 }
