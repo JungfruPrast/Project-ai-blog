e3ae9acfe17160da1a750b5cbe732b51be9e4f6b
Ezra Leong <ezrawork20@gmail.com>
2024-03-11 21:20:28 +0800

    Fixed routing issues with seo documents

--- app/(client)/seodocuments/page.tsx
+++ app/(client)/seodocuments/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { client } from "@/sanity/lib/client";
 import Header from '@/app/components/Header';
-import PostComponent from '@/app/components/PostComponent';
 import { SEO } from '@/app/utils.tsx/Interface';
+import Link from 'next/link';
+
 //set the display of content to latest published
 async function getSEO() {
   const query = `
@@ -37,3 +38,45 @@ async function getSEO() {
       </div>
     );
   }
+
+  interface Props {
+    post: SEO;
+}
+
+const PostComponent = ({post}: Props) => {
+  return (
+    <div className={cardStyle}>
+        <Link href={`/seodocuments/${post?.slug?.current}`}>
+            <h2 className='text-2xl font-bold dark:text-slate-100'>{post?.title}</h2>
+            <p className='my-2 font-semibold'>{new Date(post?.publishedAt).toDateString()}</p>
+            <p className='dark:text-gray-200 mb-4 line-clamp-2'>{post?.excerpt}</p>
+        </Link>
+
+        {/*TAGS*/}
+        <div>
+        {post?.tags?.map((tag) => (
+          <span key={tag?._id} 
+            className='mr-2 p-1 rounded-full 
+            text-sm lowercase 
+            dark:bg-gray-950 border 
+            dark:border-gray-900'>
+              #{tag?.name}</span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const cardStyle = `
+max-w-6xl
+mb-8 
+p-4
+border
+border-gray-900
+rounded-md
+shadow-md
+shadow-gray-600
+hover:bg-gray-950
+hover:text-white
+hover:dark:bg-zinc-900
+`
