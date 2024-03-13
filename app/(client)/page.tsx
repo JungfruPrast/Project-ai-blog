ac5c54a34c59033f7e15ddca922652fedab21584
Ezra Leong <ezrawork20@gmail.com>
2024-03-13 10:23:10 +0800

    Hotfix for internal server error for rootpage

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { client } from "@/sanity/lib/client";
 import Header from '../components/Header';
 import { Post } from '../utils.tsx/Interface';
-import PostComponent from '../components/PostComponent';
+import Link from 'next/link';
 
 //set the display of content to latest published
 async function getPosts() {
@@ -38,3 +38,49 @@ async function getPosts() {
       </div>
     );
   }
+
+  //adding quick hot fix for internal server error 
+
+
+interface Props {
+    post: Post;
+}
+
+const PostComponent = ({post}: Props) => {
+return (
+    <div className={cardStyle}>
+        <Link href={`/posts/${post?.slug?.current}`}>
+            <h2 className='text-2xl font-bold dark:text-slate-100'>{post?.title}</h2>
+            <p className='my-2 font-semibold'>{new Date(post?.publishedAt).toDateString()}</p>
+            <p className='dark:text-gray-200 mb-4 line-clamp-2'>{post?.excerpt}</p>
+        </Link>
+
+        {/*TAGS*/}
+        <div>
+        {post?.tags?.map((tag) => (
+          <span key={tag?._id} 
+            className='mr-2 p-1 rounded-full 
+            text-sm lowercase 
+            dark:bg-gray-950 border 
+            dark:border-gray-900'>
+              #{tag?.name}</span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+
+const cardStyle = `
+max-w-6xl
+mb-8 
+p-4
+border
+border-gray-900
+rounded-md
+shadow-md
+shadow-gray-600
+hover:bg-gray-950
+hover:text-white
+hover:dark:bg-zinc-900
+`
