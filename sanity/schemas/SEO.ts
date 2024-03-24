2a5767e88896b747e74b594b35fd18a942370c1d
Ezra Leong <ezrawork20@gmail.com>
2024-03-24 12:24:02 +0800

    I think I found a solution

--- sanity/schemas/SEO.ts
+++ sanity/schemas/SEO.ts
@@ -69,6 +69,7 @@ export const SEO = {
                     type: "block",
                     styles: [
                         { title: "Normal", value: "normal" },
+                        { title: "FirstParagraph", value: "normal"},
                         { title: "H1", value: "h1" },
                         { title: "H2", value: "h2" },
                         { title: "H3", value: "h3" },
