03263e4e4769b8cc31d1566695254d7f39d54517
Ezra Leong <ezrawork20@gmail.com>
2024-03-24 11:24:06 +0800

    still trying to fix issues

--- sanity/schemas/SEO.ts
+++ sanity/schemas/SEO.ts
@@ -73,10 +73,29 @@ export const SEO = {
                     lists: [{title:'Bullet', value:'bullet' },
                             {title: 'Number', value:'number'}
                     ],
+                  },
+                  {
+                    title: "Paragraph",
+                    name: "paragraph",
+                    type: "block",
+                    styles: [
+                        {title: "Normal", value: "normal"},
+                        {title: "H1", value: "h1"},
+                        {title: "H2", value: "h2"},
+                        {title: "H3", value: "h3"},
+                        {title: "H4", value: "h4"},
+                        {title: "H5", value: "h5"},
+                        {title: "H6", value: "h6"},
+                        {title: "Quote", value: "blockquote"},
+                    ],
+                    lists: [
+                        {title: "Bullet", value: "bullet"},
+                        {title: "Number", value: "number"}
+                    ],
                     marks: {
                       // Define the same mark definitions as your other blocks if needed
                     },
-                  },
+                }, 
                 {
                     type: "block",
                     styles: [
